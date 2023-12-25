df68166d4422564ded237541126a27d5965eeebc
1saumya <133229724+1saumya@users.noreply.github.com>
2023-12-25 15:47:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let buttons = document.querySelectorAll(".but");
+let string = "";
+
+Array.from(buttons).forEach((but) => {
+
+    but.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            document.querySelector('input').value = string;
+            
+        }
+        else if(e.target.innerHTML == "C") {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+        // console.log(e.target);
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+    
+});
